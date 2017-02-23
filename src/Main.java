fc3611cde2684c8831badf4e5eef03f08f091ca5
Gasoonjia <gasoonjia@icloud.com>
2017-02-23 13:34:16 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("103000095");
+        int i = 0x7fffffff;
+        Integer newi = i;
+        String stringI = newi.toHexString();
+        System.out.print(stringI);
+    }
+}

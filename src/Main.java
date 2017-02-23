032599d3b3bb8fb9d1f426e88429fec9ece9cae2
Gasoonjia <gasoonjia@icloud.com>
2017-02-23 13:40:33 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("103000095");
-        int i = 0x7fffffff;
-        Integer newi = i;
-        String stringI = newi.toHexString();
-        System.out.print(stringI);
     }
 }
